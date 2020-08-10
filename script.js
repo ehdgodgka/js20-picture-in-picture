a3c33e3434988d912d1fbde6d5fe3119036e8b9e
soodang <ehdgodgka@gmail.com>
2020-08-10 15:37:39 +0900

    code review modify catch bugs

--- script.js
+++ script.js
@@ -3,96 +3,90 @@ const videoSelectBtn = document.getElementById('select-btn');
 const videoCloseBtn = document.getElementById('close-btn');
 const videoElement = document.getElementById('video');
 const pipModule = document.getElementById('pip-module');
-const pipToggleState = document.getElementById('pip-toggle-btn').querySelector('input');
-// const toggleState = togglePipButton.getElementsByTagName('input');
+const pipToggleButton = document.getElementById('pip-toggle-btn').querySelector('input');
+
 let mediaStream;
-videoSelectBtn.addEventListener('click', async () => {
-  const selected = await selectMediaStream(videoElement);
-  if (selected) {
-    videoCloseBtn.style.display = 'block';
-  }
-});
-videoCloseBtn.addEventListener('click', async () => {
-  const mediaStreamTrack = mediaStream.getVideoTracks();
-  mediaStreamTrack[0].stop();
-  if (document.pictureInPictureElement) {
-    await document.exitPictureInPicture();
-  }
-  mediaStream = null;
-  videoWrapper.style.display = 'none';
-  pipModule.style.display = 'none';
-  videoCloseBtn.style.display = 'none';
-});
-console.log(pipToggleState);
-
-pipToggleState.addEventListener('change', async (event) => {
-  // If there is no element in Picture-in-Picture yet, let’s request
-  // Picture-in-Picture for the video, otherwise leave it.
-  try {
-    if (document.pictureInPictureElement) {
-      console.log('exit pip');
-      await document.exitPictureInPicture();
-      pipToggleState.checked = false;
-      videoElement.hidden = false;
-    } else {
-      console.log('pip');
-      await videoElement.requestPictureInPicture();
-      pipToggleState.checked = true;
-
-      // pipToggleState.setAttribute('checked', true);
-
-      videoElement.hidden = true;
-    }
-  } catch (err) {
-    // Video failed to enter/leave Picture-in-Picture mode.
-  }
-});
-
-const turnOffPipToggleButton = () => {
-  console.log(pipToggleState.value);
-  pipToggleState.checked = false;
-};
 
 const selectMediaStream = async (videoElement) => {
   try {
+    let clearPreStream;
+    if (mediaStream) {
+      preStreamArr = mediaStream.getVideoTracks();
+      clearPreStream = generateClearPreStream(preStreamArr);
+    }
     mediaStream = await navigator.mediaDevices.getDisplayMedia();
-
+    clearPreStream && clearPreStream();
     videoElement.srcObject = mediaStream;
     videoElement.onloadedmetadata = () => {
       videoElement.play();
     };
-    videoWrapper.style.display = 'block';
-    pipModule.style.display = 'flex';
     return true;
   } catch (error) {
     console.log('error to get media stream', error);
   }
 };
 
+const generateClearPreStream = (preStreamArr) => {
+  return () => preStreamArr[0].stop();
+};
+
+const stopMediaStreamOn = () => {
+  const mediaStreamTrack = mediaStream.getVideoTracks();
+  mediaStreamTrack && mediaStreamTrack[0].stop();
+  hideVideoAndControl();
+};
+
+const showVideoAndControl = () => {
+  videoWrapper.style.display = 'block';
+  pipModule.style.display = 'flex';
+  videoCloseBtn.style.display = 'block';
+};
+
+const hideVideoAndControl = () => {
+  videoWrapper.style.display = 'none';
+  pipModule.style.display = 'none';
+  videoCloseBtn.style.display = 'none';
+};
+
+const togglePip = () => {
+  !pipToggleButton.checked;
+  videoElement.hidden = !videoElement.hidden;
+};
+
+pipToggleButton.addEventListener('change', async (event) => {
+  try {
+    if (document.pictureInPictureElement) {
+      console.log('exit pip');
+      await document.exitPictureInPicture();
+    } else {
+      console.log('start pip');
+      await videoElement.requestPictureInPicture();
+    }
+  } catch (error) {
+    console.log('enter/exit pip mode error', error);
+  }
+});
+
+videoSelectBtn.addEventListener('click', async () => {
+  const selected = await selectMediaStream(videoElement);
+  if (selected) {
+    showVideoAndControl();
+  }
+});
+
+videoCloseBtn.addEventListener('click', async () => {
+  if (document.pictureInPictureElement) {
+    await document.exitPictureInPicture();
+  }
+  stopMediaStreamOn();
+});
+
 videoElement.addEventListener('enterpictureinpicture', function (event) {
   console.log('> Video entered Picture-in-Picture');
-
-  pipWindow = event.pictureInPictureWindow;
-  console.log(`> Window size is ${pipWindow.width}x${pipWindow.height}`);
-
-  pipWindow.addEventListener('resize', onPipWindowResize);
+  togglePip();
 });
 
 videoElement.addEventListener('leavepictureinpicture', function (event) {
-  console.log(event);
   console.log('> Video left Picture-in-Picture');
-
-  turnOffPipToggleButton();
-  // pip 만종료한 경우
-  videoElement.hidden = false;
-
-  // 완전히 종료한경우
-  console.log(mediaStream);
-  // videoWrapper.style.display = 'none';
-
-  pipWindow.removeEventListener('resize', onPipWindowResize);
+  togglePip();
 });
-
-function onPipWindowResize(event) {
-  console.log(`> Window size changed to ${pipWindow.width}x${pipWindow.height}`);
-}
